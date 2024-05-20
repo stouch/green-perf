55cacd9e238d10183368d80d2597f8fc452ef97e
stouch <syl20.touchard@gmail.com>
2024-05-20 23:53:48 +0200

    feat: Added new funds + allow table sorting

--- src/ranking-app/app/_components/RankedFunds.tsx
+++ src/ranking-app/app/_components/RankedFunds.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { FundPeriodSummary, FundSource } from "../../types/shared/fund";
 import { deltaBetweenInPercent } from "../includes/utils";
@@ -55,11 +55,57 @@ const FundLink = ({
   );
 };
 
+enum SortKey {
+  SEVEN_D = "SEVEN_D",
+  THIRTY_D = "THIRTY_D",
+  SIX_M = "SIX_M",
+  ONE_Y = "ONE_Y",
+  TWO_Y = "TWO_Y",
+}
+
+export const sortFundBy = (data: FundPeriodSummary[], sortyKey: SortKey) => {
+  data.sort((fundA, fundB) => {
+    switch (sortyKey) {
+      case SortKey.THIRTY_D:
+        return (
+          fundB.yesterday_high_value / fundB.thirty_days_low_value -
+          fundA.yesterday_high_value / fundA.thirty_days_low_value
+        );
+      case SortKey.SEVEN_D:
+        return (
+          fundB.yesterday_high_value / fundB.seven_days_low_value -
+          fundA.yesterday_high_value / fundA.seven_days_low_value
+        );
+      case SortKey.SIX_M:
+        return (
+          fundB.yesterday_high_value / fundB.six_month_low_value -
+          fundA.yesterday_high_value / fundA.six_month_low_value
+        );
+      case SortKey.ONE_Y:
+        return (
+          fundB.yesterday_high_value / fundB.one_year_low_value -
+          fundA.yesterday_high_value / fundA.one_year_low_value
+        );
+      case SortKey.TWO_Y:
+        return (
+          fundB.yesterday_high_value / fundB.two_year_low_value -
+          fundA.yesterday_high_value / fundA.two_year_low_value
+        );
+    }
+  });
+  return data;
+};
+
 export const RankedFunds = () => {
   const rankingData = useQuery<FundPeriodSummary[]>(["ranking"], async () => {
     const response = await fetch(`${nextConfig.basePath}/api/funds/ranking`);
     return await response.json();
   });
+  const [sortKey, setSortKey] = useState(SortKey.THIRTY_D);
+  const rankedData: FundPeriodSummary[] = useMemo(
+    () => (rankingData.isSuccess ? sortFundBy(rankingData.data, sortKey) : []),
+    [rankingData, sortKey]
+  );
   const wPerCol = [
     "w-10",
     "flex-1",
@@ -81,141 +127,140 @@ export const RankedFunds = () => {
           </span>
           *
         </div>
-        <div className={wPerCol[3]}>7j</div>
-        <div className={wPerCol[4]}>30j ▲</div>
-        <div className={wPerCol[5]}>6 mois</div>
-        <div className={wPerCol[6]}>1 an</div>
-        <div className={wPerCol[7]}>2 ans</div>
+        <div className={wPerCol[3]} onClick={() => setSortKey(SortKey.SEVEN_D)}>
+          7j{sortKey === SortKey.SEVEN_D ? " ▲" : ""}
+        </div>
+        <div
+          className={wPerCol[4]}
+          onClick={() => setSortKey(SortKey.THIRTY_D)}
+        >
+          30j{sortKey === SortKey.THIRTY_D ? " ▲" : ""}
+        </div>
+        <div className={wPerCol[5]} onClick={() => setSortKey(SortKey.SIX_M)}>
+          6 mois{sortKey === SortKey.SIX_M ? " ▲" : ""}
+        </div>
+        <div className={wPerCol[6]} onClick={() => setSortKey(SortKey.ONE_Y)}>
+          1 an{sortKey === SortKey.ONE_Y ? " ▲" : ""}
+        </div>
+        <div className={wPerCol[7]} onClick={() => setSortKey(SortKey.TWO_Y)}>
+          2 ans{sortKey === SortKey.TWO_Y ? " ▲" : ""}
+        </div>
       </div>
       {rankingData.isLoading ? <div className="mt-4">Chargement...</div> : ""}
-      {rankingData.isSuccess &&
-        rankingData.data.map((summary) => {
-          const oneWeekDelta = deltaBetweenInPercent({
-            from: summary.seven_days_low_value,
-            to: summary.yesterday_high_value,
-          });
-          const oneMonthDelta = deltaBetweenInPercent({
-            from: summary.thirty_days_low_value,
-            to: summary.yesterday_high_value,
-          });
-          const sixMonthDelta = deltaBetweenInPercent({
-            from: summary.six_month_low_value,
-            to: summary.yesterday_high_value,
-          });
-          const oneYearDelta = deltaBetweenInPercent({
-            from: summary.one_year_low_value,
-            to: summary.yesterday_high_value,
-          });
-          const twoYearsDelta = deltaBetweenInPercent({
-            from: summary.two_year_low_value,
-            to: summary.yesterday_high_value,
-          });
+      {rankedData.map((summary) => {
+        const oneWeekDelta = deltaBetweenInPercent({
+          from: summary.seven_days_low_value,
+          to: summary.yesterday_high_value,
+        });
+        const oneMonthDelta = deltaBetweenInPercent({
+          from: summary.thirty_days_low_value,
+          to: summary.yesterday_high_value,
+        });
+        const sixMonthDelta = deltaBetweenInPercent({
+          from: summary.six_month_low_value,
+          to: summary.yesterday_high_value,
+        });
+        const oneYearDelta = deltaBetweenInPercent({
+          from: summary.one_year_low_value,
+          to: summary.yesterday_high_value,
+        });
+        const twoYearsDelta = deltaBetweenInPercent({
+          from: summary.two_year_low_value,
+          to: summary.yesterday_high_value,
+        });
 
-          const yesterdayToLabel = `${roundValues(
-            summary.yesterday_high_value
-          )} (${dayjs(summary.yesterday_date).format("D/M/YY")})`;
-          const oneWeekDeltaLabel = `${roundValues(
-            summary.thirty_days_low_value
-          )} (${dayjs(summary.seven_days_date).format(
-            "D/M/YY"
-          )}) -> ${yesterdayToLabel})`;
-          const oneMonthDeltaLabel = `${roundValues(
-            summary.seven_days_low_value
-          )} (${dayjs(summary.thirty_days_date).format(
-            "D/M/YY"
-          )}) -> ${yesterdayToLabel})`;
-          const sixMonthDeltaLabel = `${roundValues(
-            summary.six_month_low_value
-          )} (${dayjs(summary.six_month_date).format(
-            "D/M/YY"
-          )}) -> ${yesterdayToLabel})`;
-          const oneYearDeltaLabel = `${roundValues(
-            summary.one_year_low_value
-          )} (${dayjs(summary.one_year_date).format(
-            "D/M/YY"
-          )}) -> ${yesterdayToLabel})`;
-          const twoYearDeltaLabel = `${roundValues(
-            summary.two_year_low_value
-          )} (${dayjs(summary.two_year_date).format(
-            "D/M/YY"
-          )}) -> ${yesterdayToLabel})`;
+        const yesterdayToLabel = `${roundValues(
+          summary.yesterday_high_value
+        )} (${dayjs(summary.yesterday_date).format("D/M/YY")})`;
+        const oneWeekDeltaLabel = `${roundValues(
+          summary.thirty_days_low_value
+        )} (${dayjs(summary.seven_days_date).format(
+          "D/M/YY"
+        )}) -> ${yesterdayToLabel})`;
+        const oneMonthDeltaLabel = `${roundValues(
+          summary.seven_days_low_value
+        )} (${dayjs(summary.thirty_days_date).format(
+          "D/M/YY"
+        )}) -> ${yesterdayToLabel})`;
+        const sixMonthDeltaLabel = `${roundValues(
+          summary.six_month_low_value
+        )} (${dayjs(summary.six_month_date).format(
+          "D/M/YY"
+        )}) -> ${yesterdayToLabel})`;
+        const oneYearDeltaLabel = `${roundValues(
+          summary.one_year_low_value
+        )} (${dayjs(summary.one_year_date).format(
+          "D/M/YY"
+        )}) -> ${yesterdayToLabel})`;
+        const twoYearDeltaLabel = `${roundValues(
+          summary.two_year_low_value
+        )} (${dayjs(summary.two_year_date).format(
+          "D/M/YY"
+        )}) -> ${yesterdayToLabel})`;
 
-          return (
-            <div
-              key={summary.name}
-              className="flex flex-row gap-3 my-4 text-sm"
-            >
-              <div className={wPerCol[0]}>{summary.position}</div>
-              <div className={wPerCol[1]}>
-                {summary.name}
-                <br />
-                <FundLink source={summary.source} fundId={summary.id}>
-                  <small className="text-gray-200">
-                    {summary.source} - {summary.id}
-                  </small>
-                </FundLink>
-              </div>
-              <div className={wPerCol[2]}>
-                {summary.is_greenfin ? "🌱" : "❌"}
-              </div>
-              <div className={`${wPerCol[3]} flex flex-row gap-2 items-center`}>
-                <RankingTooltip content={oneWeekDeltaLabel}>
-                  <span
-                    className={oneWeekDelta > 0 ? "text-green" : "text-red"}
-                  >
-                    {oneWeekDelta > 0 ? "+" : ""}
-                    {oneWeekDelta}%
-                  </span>
-                  {/*<small>(7j)</small>*/}
-                </RankingTooltip>
-              </div>
-              <div className={`${wPerCol[4]} flex flex-row items-center`}>
-                <RankingTooltip content={oneMonthDeltaLabel}>
-                  <span
-                    className={oneMonthDelta > 0 ? "text-green" : "text-red"}
-                  >
-                    {oneMonthDelta > 0 ? "+" : ""}
-                    {oneMonthDelta}%
-                  </span>
-                  {/*<small>(30j)</small>*/}
-                </RankingTooltip>
-              </div>
-              <div className={`${wPerCol[5]} flex flex-row gap-2 items-center`}>
-                <RankingTooltip content={sixMonthDeltaLabel}>
-                  <span
-                    className={sixMonthDelta > 0 ? "text-green" : "text-red"}
-                  >
-                    {sixMonthDelta > 0 ? "+" : ""}
-                    {sixMonthDelta}%
-                  </span>
-                  {/*<small>(6 mois)</small>*/}
-                </RankingTooltip>
-              </div>
-              <div className={`${wPerCol[6]} flex flex-row gap-2 items-center`}>
-                <RankingTooltip content={oneYearDeltaLabel}>
-                  <span
-                    className={oneYearDelta > 0 ? "text-green" : "text-red"}
-                  >
-                    {oneYearDelta > 0 ? "+" : ""}
-                    {oneYearDelta}%
-                  </span>
-                  {/*<small>(1a)</small>*/}
-                </RankingTooltip>
-              </div>
-              <div className={`${wPerCol[7]} flex flex-row gap-2 items-center`}>
-                <RankingTooltip content={twoYearDeltaLabel}>
-                  <span
-                    className={twoYearsDelta > 0 ? "text-green" : "text-red"}
-                  >
-                    {twoYearsDelta > 0 ? "+" : ""}
-                    {twoYearsDelta}%
-                  </span>
-                  {/*<small>(2a)</small>*/}
-                </RankingTooltip>
-              </div>
+        return (
+          <div key={summary.name} className="flex flex-row gap-3 my-4 text-sm">
+            <div className={wPerCol[0]}>{summary.position}</div>
+            <div className={wPerCol[1]}>
+              {summary.name}
+              <br />
+              <FundLink source={summary.source} fundId={summary.id}>
+                <small className="text-gray-200">
+                  {summary.source} - {summary.id}
+                </small>
+              </FundLink>
             </div>
-          );
-        })}
+            <div className={wPerCol[2]}>
+              {summary.is_greenfin ? "🌱" : "❌"}
+            </div>
+            <div className={`${wPerCol[3]} flex flex-row gap-2 items-center`}>
+              <RankingTooltip content={oneWeekDeltaLabel}>
+                <span className={oneWeekDelta > 0 ? "text-green" : "text-red"}>
+                  {oneWeekDelta > 0 ? "+" : ""}
+                  {oneWeekDelta}%
+                </span>
+                {/*<small>(7j)</small>*/}
+              </RankingTooltip>
+            </div>
+            <div className={`${wPerCol[4]} flex flex-row items-center`}>
+              <RankingTooltip content={oneMonthDeltaLabel}>
+                <span className={oneMonthDelta > 0 ? "text-green" : "text-red"}>
+                  {oneMonthDelta > 0 ? "+" : ""}
+                  {oneMonthDelta}%
+                </span>
+                {/*<small>(30j)</small>*/}
+              </RankingTooltip>
+            </div>
+            <div className={`${wPerCol[5]} flex flex-row gap-2 items-center`}>
+              <RankingTooltip content={sixMonthDeltaLabel}>
+                <span className={sixMonthDelta > 0 ? "text-green" : "text-red"}>
+                  {sixMonthDelta > 0 ? "+" : ""}
+                  {sixMonthDelta}%
+                </span>
+                {/*<small>(6 mois)</small>*/}
+              </RankingTooltip>
+            </div>
+            <div className={`${wPerCol[6]} flex flex-row gap-2 items-center`}>
+              <RankingTooltip content={oneYearDeltaLabel}>
+                <span className={oneYearDelta > 0 ? "text-green" : "text-red"}>
+                  {oneYearDelta > 0 ? "+" : ""}
+                  {oneYearDelta}%
+                </span>
+                {/*<small>(1a)</small>*/}
+              </RankingTooltip>
+            </div>
+            <div className={`${wPerCol[7]} flex flex-row gap-2 items-center`}>
+              <RankingTooltip content={twoYearDeltaLabel}>
+                <span className={twoYearsDelta > 0 ? "text-green" : "text-red"}>
+                  {twoYearsDelta > 0 ? "+" : ""}
+                  {twoYearsDelta}%
+                </span>
+                {/*<small>(2a)</small>*/}
+              </RankingTooltip>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/ranking-app/app/page.tsx
+++ src/ranking-app/app/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { FundChart } from "./_components/FundChart";
 import { RankedFunds } from "./_components/RankedFunds";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "Mon épargne verte";
+  }, []);
   return (
-    <main className="min-h-screen py-24 px-4 md:px-24 overflow-x-auto md:overflow-x-hidden">
+    <main className="min-h-screen py-20 px-4 md:px-24 overflow-x-auto md:overflow-x-hidden">
       <div className="w-[860px] mx-auto">
         <div className="mt-6 text-gray-300">
-          {`Cessons d'investir dans les combustibles fossiles.`}
+          {`Mettre son épargne au vert 🌳`}
         </div>
         <div className="mt-2 text-gray-500">
           {`Tous les fonds "verts" listés ci-dessous sont des OPCVM, 
             accessibles à n'importe quel particulier 
-            par souscription à des unités de compte sur une assurance-vie par exemple.`}
+            par souscription à des unités de compte sur une assurance-vie par exemple. 
+            Les valeurs sont actualisées une fois par jour.`}
         </div>
         <div className="mt-2 text-gray-500">
           {`Parlez-en à votre conseiller en patrimoine ou à votre banquier.`}
